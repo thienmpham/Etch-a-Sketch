7561bca38ff886473cea0201ae4cdd1a3aefdd63
Tensen <thienmpham@gmail.com>
2025-02-06 12:21:37 -0500

    Change size of squares to perfect squares
    Makes the squares more pleasing visually and added
    padding-bottom to each square because padding
    calculates based on the containers width rather than
    the containers height thus making the height and
    width equal value.

--- sketch.js
+++ sketch.js
@@ -1,1 +1,19 @@
 // Append square divs inside the div container 16x16
+
+function appendSquare() {
+
+
+    let container = document.querySelector('.container');
+    //for loop for 16x16 square 
+    for (let i = 1; i <= 256; i++) {
+        // create div 
+        let div = document.createElement('div');
+        // set the className for each new div 
+        div.setAttribute('class', 'square');
+        // append square to container
+        container.appendChild(div);
+
+
+    }
+}
+appendSquare();
