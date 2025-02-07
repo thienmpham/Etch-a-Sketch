30643369492922b99799273e3fca633ac9765ee9
Tensen <thienmpham@gmail.com>
2025-02-07 11:33:38 -0500

    Plan out functions

--- sketch.js
+++ sketch.js
@@ -1,9 +1,34 @@
-// Append square divs inside the div container 16x16
+
+
+function onButtonPress() {
+    // on Generate button press call function promptAmount()
+
+
+}
+
+function removeGrid() {
+
+}
+
+function promptAmount() {
+    //prompt asking for how many squres per side for new grid
+    //  ex. 64 is 64x64 grid 
+
+
+    //add eventListener to wait for prompt response
+    // and call the function appendSquare()
+    // and call the function removeGrid(); 
+
+
+}
 
 function appendSquare() {
-
-
     let container = document.querySelector('.container');
+
+    //calculations 
+    // total area is equal to input times input 
+    // side length is equal to input divided by 100 
+
     //for loop for 16x16 square 
     for (let i = 1; i <= 256; i++) {
         // create div 
