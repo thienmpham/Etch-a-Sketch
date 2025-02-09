7b759ced547a8118b3cf5ef7e86f4dde231c68d0
Tensen <thienmpham@gmail.com>
2025-02-09 16:15:00 -0500

    Add prompt logic to promptAmount() function
    Adding prompt method and having Number() constructor
    with an if statement to see if input of the prompt
    is not a negative number and is less or equal to 100
    to allow for no errors when calling appendSquares()

--- sketch.js
+++ sketch.js
@@ -2,9 +2,14 @@
 
 function onButtonPress() {
     // on Generate button press call function promptAmount()
+    let button = document.querySelector('#generate');
+    button.addEventListener('click', function () {
+        promptAmount();
 
+    })
 
 }
+onButtonPress();
 
 function removeGrid() {
 
@@ -13,24 +18,34 @@ function removeGrid() {
 function promptAmount() {
     //prompt asking for how many squres per side for new grid
     //  ex. 64 is 64x64 grid 
+    let response = Number(prompt('How many squares per side?'));
 
-
-    //add eventListener to wait for prompt response
-    // and call the function appendSquare()
-    // and call the function removeGrid(); 
+    // if response is a number greater than 0
+    // and less than or equal to 100
+    // then call apendSquare() function 
+    if (response > 0 && response <= 100) {
+        appendSquare(response)
+        console.log('response is a number!')
+    } else {
+        console.log('Invalid response, Try again!')
+        return;
+    }
 
 
 }
 
-function appendSquare() {
+function appendSquare(response) {
     let container = document.querySelector('.container');
 
     //calculations 
     // total area is equal to input times input 
-    // side length is equal to input divided by 100 
+    let area = response * response;
+
+    // side length of squareis equal to input divided by 100 
+    let length = response / 100;
 
     //for loop for 16x16 square 
-    for (let i = 1; i <= 256; i++) {
+    for (let i = 1; i <= area; i++) {
         // create div 
         let div = document.createElement('div');
         // set the className for each new div 
@@ -41,4 +56,3 @@ function appendSquare() {
 
     }
 }
-appendSquare();
